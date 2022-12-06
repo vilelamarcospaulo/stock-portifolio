640e584ecefd564c2b84129c7f3176b82851d133
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2022-12-06 18:33:32 -0300

    chore: scaffold stock module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { StockModule } from './stock/stock.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StockModule],
   controllers: [AppController],
   providers: [AppService],
 })
