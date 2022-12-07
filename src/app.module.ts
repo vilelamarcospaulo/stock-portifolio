a8877aa959ea1d1ec385fd5d4990c7c29cc515ad
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2022-12-07 00:01:21 -0300

    chore: clean up project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
   imports: [PrismaModule, PortfolioModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
